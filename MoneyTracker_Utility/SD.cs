785e5b9b4f38a8e168a03312198875d34f9b7ef0
Hassan Koraim <hassankoraim2@gmail.com>
2025-10-24 18:44:54 +0300

    Add  MoneyTracker_Utility

---
+++ MoneyTracker_Utility/SD.cs
@@ -0,0 +1,20 @@
+﻿namespace MoneyTracker_Utility
+{
+    public static class SD
+    {
+        public enum RecurrenceType
+        {
+            None = 0,
+            Daily = 1,
+            Weekly = 2,
+            Monthly = 3,
+            Yearly = 4
+        }
+        public enum CategoryType
+        {
+            Income ,
+            Expense
+        }
+    }
+    
+}
