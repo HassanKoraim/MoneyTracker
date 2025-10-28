a09b44c50c7c0feb101193643452ff95fcdf4418
Hassan Koraim <hassankoraim2@gmail.com>
2025-10-28 20:56:54 +0300

    add transaction CreateDto

---
+++ MoneyTracker_API/DTOs/TransactionCreateDto.cs
@@ -0,0 +1,19 @@
+﻿using static MoneyTracker_Utility.SD;
+
+namespace MoneyTracker_API.DTOs
+{
+    public class TransactionCreateDto
+    {
+        public decimal Amount { get; set; }
+        public string Description { get; set; } = string.Empty;
+        public TransactionType TransactionType { get; set; }
+        public DateTime TransactionDate { get; set; }
+        public int CategoryId { get; set; }
+        public int? ParentCategoryId { get; set; }
+        public int PaymentMethodId { get; set; }
+        public bool IsRecurring { get; set; }
+        public RecurrenceType? RecurrenceType { get; set; }
+        public DateTime? RecurrenceEndDate { get; set; }
+        public string? ImageUrl { get; set; }
+    }
+}
